18d636580ff39cc295cad54083cbb47f27d6e602
Jimmy Pautz <jpautz@dig.ms>
2018-01-22 08:49:18 -0600

    Super minor update to options

--- settings/options.js
+++ settings/options.js
@@ -2,6 +2,6 @@ function saveOptions() {
 	localStorage['amzn_code'] = document.getElementById('amzncode').value;
 }
 document.addEventListener('DOMContentLoaded', function () {
-	document.getElementById('amzncode').value = localStorage['amzn_code'];
+	document.getElementById('amzncode').value = localStorage['amzn_code'] || "";
 	document.getElementById('amzncode').addEventListener("input", saveOptions);
 });
